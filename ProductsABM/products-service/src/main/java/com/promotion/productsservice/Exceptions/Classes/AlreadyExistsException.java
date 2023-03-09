951bfd90ea81d5e94ceb8cb3b09f87c50f1a9987
Franco Ortega <ortegafran97@gmail.com>
2023-03-09 15:55:19 -0300

    Exceptions Handler

---
+++ ProductsABM/products-service/src/main/java/com/promotion/productsservice/Exceptions/Classes/AlreadyExistsException.java
@@ -0,0 +1,5 @@
+package com.promotion.productsservice.Exceptions.Classes;
+
+public class AlreadyExistsException extends RuntimeException{
+    public AlreadyExistsException(String message){super(message);}
+}
---
+++ ProductsABM/products-service/src/main/java/com/promotion/productsservice/Exceptions/Classes/ValidationException.java
@@ -0,0 +1,5 @@
+package com.promotion.productsservice.Exceptions.Classes;
+
+public class ValidationException extends RuntimeException{
+    public ValidationException(String message){super(message);}
+}
