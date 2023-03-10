136bf3539b130fd889e2100a5cbe7a42f82af39d
Franco Ortega <ortegafran97@gmail.com>
2023-03-10 10:34:14 -0300

    Exception added

---
+++ ProductsABM/products-service/src/main/java/com/promotion/productsservice/Exceptions/Classes/CouldntCompleteException.java
@@ -0,0 +1,5 @@
+package com.promotion.productsservice.Exceptions.Classes;
+
+public class CouldntCompleteException extends RuntimeException{
+    public CouldntCompleteException(String message){super(message);}
+}
