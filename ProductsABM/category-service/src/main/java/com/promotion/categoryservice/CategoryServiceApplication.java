bad7eea43fc24737947a2dc996425bde140d4e41
georgina.crespo <georgina.crespo@globallogic.com>
2023-03-28 18:09:44 -0300

    Primer implementacion de Swagger, category-service

--- ProductsABM/category-service/src/main/java/com/promotion/categoryservice/CategoryServiceApplication.java
+++ ProductsABM/category-service/src/main/java/com/promotion/categoryservice/CategoryServiceApplication.java
@@ -3,13 +3,27 @@ package com.promotion.categoryservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
 @EnableEurekaClient
+@Configuration
+@EnableSwagger2
 public class CategoryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CategoryServiceApplication.class, args);
 	}
 
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.promotion.categoryservice")).build();
+	}
 }
--- ProductsABM/category-service/src/main/java/com/promotion/categoryservice/controller/CategoryController.java
+++ ProductsABM/category-service/src/main/java/com/promotion/categoryservice/controller/CategoryController.java
@@ -1,6 +1,8 @@
 package com.promotion.categoryservice.controller;
 
 import com.promotion.categoryservice.entity.Category;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,9 +10,10 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-
+@Api(value = "categorias", description = "Operaciones relacionadas con categorias")
 public interface CategoryController {
 
+    @ApiOperation(value = "Obtener todas las categorias", response = Category.class, responseContainer = "List")
     @GetMapping()
     ResponseEntity<List<Category>> findAll();
 
